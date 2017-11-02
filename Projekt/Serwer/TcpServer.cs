fc8a171da267d135d6c528a3ecd94dd0cfbdbcbd
Aymaro <lodrigos123@gmail.com>
2017-11-02 19:32:54 +0100

    Zmiana w pliku serwer
    W tym momencie działa odczytywanie danych z czytnika w pełnej funkcji.
    Serwer odczytuje taki dane jak:
    - UID karty
    - czas wyslania wiadomości (EPOCH)
    - ID czytnika (narazie jest tylko jeden)

---
+++ Projekt/Serwer/TcpServer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Projekt
+{
+    class TcpServer
+    {
+        private int HostPort = 13000;
+        //konstruktory
+        public TcpServer()
+        {
+
+        }
+        public TcpServer(int _HostPort)
+        {
+            HostPort = _HostPort;
+        }
+        
+
+
+        public void Listen()
+        {
+            //Stworzenie nasluchiwacza
+            TcpListener server = null;
+
+            try
+            {
+                
+                
+                //Listener dostaje dane o ip i porcie
+                server = new TcpListener(IPAddress.Any, HostPort);
+
+                //start serwera nasluchiwania
+                server.Start();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+                string UID = null;
+                string EPOCH = null;
+                string RDR = null;
+
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Raw data: {0}", data);
+
+                        RDR = data.Remove(3);
+                        data = data.Remove(0, 4);
+                        EPOCH = null;
+                        UID = data.Remove(16);
+                        UID = UID.Replace("0x", "");
+
+                     
+                        for (int j = 17; j < data.Length; j++)
+                        {
+                            EPOCH += data[j];
+                        }
+
+                        Console.WriteLine("RDR: {0}", RDR);
+                        Console.WriteLine("UID: {0}", UID);
+                        Console.WriteLine("EPOCH: {0}", EPOCH);
+
+                    }
+                    client.Close();
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+            Console.Read();
+        }
+
+    }
+}
